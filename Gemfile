23d4b43c2f847c5611a8465f3ac3b11f7e63c77b
tamimfares <tamimfares@me.com>
2016-12-30 20:33:47 +0200

    Cards app with full CRUD functionality and Styling

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.1.0'
 end
 gem 'simple_form'
+gem "bootstrap_flash_messages", "~> 1.0.1"
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,9 @@
 class CardsController < ApplicationController
 
+  def index
+    @cards = Card.all.order("created_at DESC")
+  end
+
   def new
     @card = Card.new
   end
@@ -8,8 +12,9 @@ class CardsController < ApplicationController
     @card = Card.new(card_params)
 
     if @card.save
-      redirect_to @card
+      redirect_to(@card, :flash => :success)
     else
+      flash_now!(:error)
       render 'new'
     end
   end
@@ -18,7 +23,26 @@ class CardsController < ApplicationController
     @card = Card.find(params[:id])
   end
 
+  def edit
+    @card = Card.find(params[:id])
+  end
 
+  def update
+    @card = Card.find(params[:id])
+
+    if @card.update(card_params)
+      redirect_to(@card, :flash => :success)
+    else
+      flash_now!(:error)
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to(cards_path, :flash => :warning)
+  end
 
   private
     def card_params
